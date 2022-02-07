f865f3e480b7c001f0e47198dd6c6ed16047aed7
orisa07037616291 <orisakwepeter@gmail.com>
2022-02-07 11:35:35 +0100

    new commit

--- Form.js
+++ Form.js
@@ -1,18 +1,15 @@
-function filter( ) {
-
-  let value = document.getElementById("searchInput").value.toUpperCase ( );
+function filter() {
+  let value = document.getElementById("searchInput").value.toUpperCase();
   var names = document.getElementById("names");
   var rows = names.getElementsByTagName("tr");
 
   for (i = 0; i < rows.length; i++) {
-let column = rows[i].getElementsByTagName("td")[2];
-let language = column.textContent;
+    let column = rows[i].getElementsByTagName("td")[2];
+    let language = column.textContent;
 
-
-     rows[i].style.display =
-     language.toUpperCase( ).indexOf(value) > -1 ? "" : "none";
+    rows[i].style.display =
+      language.toUpperCase().indexOf(value) > -1 ? "" : "none";
   }
 }
 
 document.getElementById("searchInput").addEventListener("keyup", filter);
-
